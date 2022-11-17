7eccc11efb91ead4ea0ff563a3942970b811bad0
DepressedGoat <iasonas96@gmail.com>
2022-11-17 19:44:58 +0200

    Add files via upload

---
+++ Prime/prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Simple C code that checks and prints all prime numbers from 1 to 100
+ 
+int main()
+{
+  int i, a = 1, count; 
+  
+
+  while(a <= 100)
+  {
+    count = 0;
+    i = 2;
+    while(i <= a/2)
+    {
+      if(a%i == 0)
+      {
+        count++;
+	break;
+      }
+      i++;	
+    }	
+    if(count == 0 && a != 1 )
+    {
+	printf(" %d ", a);
+    }
+    a++; 
+  }
+  return 0;
+}
